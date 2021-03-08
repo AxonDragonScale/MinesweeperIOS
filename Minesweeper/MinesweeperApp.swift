5ae3460c5c71cfd752ab766e421f287f8d9146e2
Ronak Harkhani <ronak.harkhani@phonepe.com>
2021-03-09 00:19:21 +0530

    Initial Commit

---
+++ Minesweeper/MinesweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinesweeperApp.swift
+//  Minesweeper
+//
+//  Created by Ronak Harkhani on 09/03/21.
+//
+
+import SwiftUI
+
+@main
+struct MinesweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
